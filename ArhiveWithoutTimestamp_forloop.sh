6a048d6faaf4b66c6cb652ca50eab1a571fb26d7
Priyanka Patil <priyankarajupatil@outlook.com>
2021-10-09 19:09:55 +0530

    Update ArhiveWithoutTimestamp_forloop.sh

--- ArhiveWithoutTimestamp_forloop.sh
+++ ArhiveWithoutTimestamp_forloop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # This script is created to backup files without adding timestamp to the files while moving.
-# Using for loop
-# Moves CSV files in the ARCHIVE dir
-# author - priyanka patil
-for i in `ls /datashare/BAR/CLM/*.CSV`
+# Using for loop, you can move CSV files in the ARCHIVE dir
+for i in `ls /<<<new_path>>>/*.CSV`
 do
-`mv $i /datashare/BAR/CLM/ARCHIVE/ 2>/dev/null`
+`mv $i /<<<path>>>/ARCHIVE/ 2>/dev/null`
 done
