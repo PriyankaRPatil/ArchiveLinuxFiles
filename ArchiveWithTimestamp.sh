c6a4b2dbf2d16900dc4d921472c2c2060daf2f95
Priyanka Patil <priyankarajupatil@outlook.com>
2021-10-09 19:10:59 +0530

    Update ArchiveWithTimestamp.sh

--- ArchiveWithTimestamp.sh
+++ ArchiveWithTimestamp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # This script is created to backup files and by adding timestamp to the files while moving.
 # Moves CSV files in the ARCHIVE dir
-# author - priyanka patil
 for i in `ls /path/*.CSV`
 do
         filename=`basename $i`
-        mv $filename /path/ARCHIVE/`date +%Y%m%d_%H%M`_$filename 2>/dev/null
+        mv $filename /<<<path>>>/`date +%Y%m%d_%H%M`_$filename 2>/dev/null
 done
